3d8fb4ff53da526f8bc2d582a9173ddbeb3e2e97
Rob <rob.bog@list.ru>
2020-05-02 17:13:45 +0300

    [ANY] Удаление копирайтов

--- mrvn_modules/base.py
+++ mrvn_modules/base.py
@@ -1,19 +1,3 @@
-#  The MIT License (MIT)
-#
-#  Copyright © «2020» «ALFO:WorkS, Iterator»
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-#  The MIT License (MIT)
-#
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-#
 import io
 import re
 import time
@@ -28,10 +12,6 @@ from modular import Module, Command, CommandContext, CommandResult, EmbedType, D
 @mrvn_module("BaseModule", "Модуль с основными функциями.")
 class BaseModule(Module):
     async def on_enable(self):
-        self.bot.module_handler.add_param("aqbb", 1488)
-        self.bot.module_handler.add_param("test2", [])
-        self.bot.module_handler.add_param("test3", {})
-
         @mrvn_command(self, "cmds", "Список команд, их аргументы и описание.", keys_desc=["--all"])
         class HelpCommand(Command):
             async def execute(self, ctx: CommandContext) -> CommandResult:
