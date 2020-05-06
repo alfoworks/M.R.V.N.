ac0185b19ee2a2071358e25c11efc2b6a0f4e1a1
Rob <rob.bog@list.ru>
2020-05-06 05:21:43 +0300

    [FEATURE] Кастомный текст в bot_status.py

--- mrvn_modules/bot_status.py
+++ mrvn_modules/bot_status.py
@@ -10,21 +10,24 @@ from modular import Module, LanguageUtils
 @mrvn_module("BotStatus", "Установка статуса бота, отображение аптайма в нём.")
 class BotStatusModule(Module):
     async def status_update_task(self):
-        beu = False
+        custom_text = False
 
         while True:
-            status = "🐖 Беубасс" if beu else "🕒 Аптайм: %s" % LanguageUtils.formatted_duration(
+            status = self.bot.module_handler.get_param(
+                "bot_status_custom_text") if custom_text else "🕒 Аптайм: %s" % LanguageUtils.formatted_duration(
                 time.time() - self.bot.start_time)
             await self.bot.change_presence(status=discord.Status.idle,
                                            activity=discord.Activity(
                                                name=status,
                                                type=discord.ActivityType.listening))
 
-            beu = not beu
+            custom_text = not custom_text
 
             await asyncio.sleep(60)
 
     async def on_enable(self):
+        self.bot.module_handler.add_param("bot_status_custom_text", "🐖 Беубасс")
+
         self.logger.info("Запуск таска обновления статуса...")
 
         await self.bot.module_handler.add_background_task(self.status_update_task(), self)
