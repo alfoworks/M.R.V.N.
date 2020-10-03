617e6853803dceed8d1315d9382145668ad5f035
ov3rwrite <38213271+ov3rwrite@users.noreply.github.com>
2020-10-03 13:27:51 +0300

    [FEATURE] Добавление параметров в конфиг для новой фичи

--- mrvn_config.py
+++ mrvn_config.py
@@ -1,7 +1,8 @@
 class MrvnConfig:
     modules_dirs = ["mrvn_modules"]  # Папка, откуда будут загружаться модули.
     ignored_modules = [""]  # ИМЕНА ФАЙЛОВ модулей, которые будут проигнорированы при загрузке.
-
+    github_login = ""
+    github_password = ""
     guild_whitelist = []  # Белый список серверов
 
     @staticmethod
